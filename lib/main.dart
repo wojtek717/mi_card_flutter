14a28ddcdb7a6a2f2677dcd946124254ec604c0e
wojtek717 <wojciechkonury@gmail.com>
2019-09-13 20:16:30 +0200

    Avatar and name

--- lib/main.dart
+++ lib/main.dart
@@ -11,39 +11,20 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
             children: <Widget>[
-              Container(
-                color: Colors.red,
-                width: 100.0,
-                child: Text('Row 1'),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.red,
+                backgroundImage: NetworkImage(
+                    'https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-9/54727578_2190483284365480_8636337435845853184_o.jpg?_nc_cat=101&_nc_oc=AQni42Fx2erhW0XYENPQISX-RwHqaJk-ZMtU-4SGv_vZKS5Ms_qmIOADgTaTdwYsj94&_nc_ht=scontent-waw1-1.xx&oh=6626a3cd6a52b7c95a691e0c3f93d82f&oe=5DFA1DC8'),
               ),
-
-              Container(
-                width: 100.0,
-                height: 200.0,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      color: Colors.yellow,
-                      height: 100.0,
-                    ),
-
-                    Container(
-                      color: Colors.green,
-                      height: 100.0,
-                    ),
-                  ],
-                ),
-              ),
-
-              Container(
-                color: Colors.blue,
-                width: 100.0,
-                child: Text('Row 3'),
+              Text(
+                'Wojciech Konury',
+                style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
               ),
             ],
           ),
