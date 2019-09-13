bb0494ed7227e507cc9da7201ea8999e9cd63082
wojtek717 <wojciechkonury@gmail.com>
2019-09-13 19:24:22 +0200

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,54 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                color: Colors.red,
+                width: 100.0,
+                child: Text('Row 1'),
+              ),
+
+              Container(
+                width: 100.0,
+                height: 200.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      color: Colors.yellow,
+                      height: 100.0,
+                    ),
+
+                    Container(
+                      color: Colors.green,
+                      height: 100.0,
+                    ),
+                  ],
+                ),
+              ),
+
+              Container(
+                color: Colors.blue,
+                width: 100.0,
+                child: Text('Row 3'),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
