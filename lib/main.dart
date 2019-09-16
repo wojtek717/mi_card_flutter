a97c7102326e0c96c383e8dc812c77f95f0bac92
wojtek717 <wojciechkonury@gmail.com>
2019-09-16 19:22:00 +0200

    Gradient and github

--- lib/main.dart
+++ lib/main.dart
@@ -8,80 +8,107 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundColor: Colors.red,
-                backgroundImage: NetworkImage(
-                    'https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-9/54727578_2190483284365480_8636337435845853184_o.jpg?_nc_cat=101&_nc_oc=AQni42Fx2erhW0XYENPQISX-RwHqaJk-ZMtU-4SGv_vZKS5Ms_qmIOADgTaTdwYsj94&_nc_ht=scontent-waw1-1.xx&oh=6626a3cd6a52b7c95a691e0c3f93d82f&oe=5DFA1DC8'),
-              ),
-              Text(
-                'Wojciech Konury',
-                style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40,
+      home: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0, 1],
+              colors: [Colors.teal, Colors.blue[400]]),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 60.0,
+                  backgroundColor: Colors.red,
+                  backgroundImage: NetworkImage(
+                      'https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-9/54727578_2190483284365480_8636337435845853184_o.jpg?_nc_cat=101&_nc_oc=AQni42Fx2erhW0XYENPQISX-RwHqaJk-ZMtU-4SGv_vZKS5Ms_qmIOADgTaTdwYsj94&_nc_ht=scontent-waw1-1.xx&oh=6626a3cd6a52b7c95a691e0c3f93d82f&oe=5DFA1DC8'),
+                ),
+                Text(
+                  'Wojciech Konury',
+                  style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                    color: Colors.teal[100],
                     fontWeight: FontWeight.bold,
-                    color: Colors.white),
-              ),
-              Text(
-                'FLUTTER DEVELOPER',
-                style: TextStyle(
-                  fontFamily: 'Source Sans Pro',
-                  fontSize: 20.0,
-                  color: Colors.teal[100],
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 3.0,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 180.0,
-                child: Divider(
-                  color: Colors.teal[100],
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.teal[900],
+                    letterSpacing: 3.0,
                   ),
-                  title: Text(
-                    '+48 123 456 789',
-                    style: TextStyle(
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20.0,
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 180.0,
+                  child: Divider(
+                    color: Colors.teal[100],
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
                       color: Colors.teal[900],
+                    ),
+                    title: Text(
+                      '+48 123 456 789',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                        color: Colors.teal[900],
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    color: Colors.teal[900],
-                  ),
-                  title: Text(
-                    'wojciechkonury@gmail.com',
-                    style: TextStyle(
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20.0,
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
                       color: Colors.teal[900],
+                    ),
+                    title: Text(
+                      'wojciechkonury@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                        color: Colors.teal[900],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.web,
+                      color: Colors.teal[900],
+                    ),
+                    title: Text(
+                      'https://github.com/wojtek717',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                        color: Colors.teal[900],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
